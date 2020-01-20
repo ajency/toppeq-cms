5d1ffe6c0af24b4c8efdfad5e0a8f72d37990ffa
Ankita <ankita@ajency.in>
2020-01-20 16:30:16 +0530

    css changes for smaller screens

--- wp-content/themes/toppeq/template-contact.php
+++ wp-content/themes/toppeq/template-contact.php
@@ -31,9 +31,9 @@ if (have_posts()) {
 			<div class="col-md-7  contact-form">
 				<div class="container">
 				  	<ul class="nav nav-tabs">
-				    	<li class="active"><a href="#tab1" data-toggle="tab"><i class="fa fa-user" aria-hidden="true"></i></a></li>  
-				    	<li><a href="#tab2" data-toggle="tab"><i class="fa fa-user" aria-hidden="true"></i></a></li>
-				    	<li><a href="#tab3" data-toggle="tab"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+				    	<li class="active"><a href="#tab1" data-toggle="tab"><i class="fa fa-user" aria-hidden="true"></i></a><span class="mt-2 text-center">Contact HR</span></li>  
+				    	<li><a href="#tab2" data-toggle="tab"><i class="fa fa-user" aria-hidden="true"></i></a><span class="mt-2 text-center">Dummy Text</span></li>
+				    	<li><a href="#tab3" data-toggle="tab"><i class="fa fa-user" aria-hidden="true"></i></a><span class="mt-2 text-center">Dummy Text</span></li>
 				  	</ul>
 
 				  	<div class="tab-content mt-8 position-relative">
