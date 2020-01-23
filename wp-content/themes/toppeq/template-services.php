acc7c407922d6c7e8975d5d44b1f4411fff9644e
Ankita <ankita@ajency.in>
2020-01-23 11:03:40 +0530

    header ui

---
+++ wp-content/themes/toppeq/template-services.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Template Name: Service-page
+ */
+
+get_header(); ?>
+
+
+	<div class="services-page">
+		<div class="container w-980">
+			<div class="detailed-page">
+				<div class="row services-block services-01 left">
+					<div class="number-overlay"> 01 </div>
+					<div class="d-block">
+						<div class="title">
+							<h2 class="pb-2">CAP TABLE <br> MANAGEMENT</h2>
+						</div>
+						<div class="content">
+							<p>From shareholders to percentage of holding and types of securities, TOPPEQ’s cloud-based platform digitises and secures your equity data and makes it both instantly accessible and easy to understand.</p>
+						</div>
+					</div>
+					<div class="outer-box-shares">
+						<table>
+							<th>
+								<td>Shareholder Name</td>
+								<td>Number of Shares</td>
+							</th>
+							<tr>
+								<td>John Appleseed</td>
+								<td>1,400</td>
+							</tr>
+							<tr>
+								<td>John Doe</td>
+								<td>1,600</td>
+							</tr>
+						</table>
+					</div>
+				</div>
+				<div class="row services-block services-02 m-5 text-right right">
+				<div class="number-overlay"> 02 </div>
+					<div class="title w-100">
+						<h2 class="pb-2">PORTFOLIO ANALYTICS <br> AND SCENARIO <br> MODELING</h2>
+					</div>
+					<div class="content">
+						<p>We deep-dive into big data across sectors to help deliver critical insights that optimise your funding round decisions.</p>
+					</div>
+				</div>
+				<div class="row  services-block services-03 m-5 left">
+				<div class="number-overlay"> 03 </div>
+					<div class="title">
+						<h2 class="pb-2">DUE DILIGENCE & <br> VALUATION</h2>
+					</div>
+					<div class="content">
+						<p>Our integrated suite of tools provides accurate and defensible audit-ready valuations, and our relentless focus on compliance adherence  speeds up the due diligence process. </p>
+					</div>
+				</div>
+				<div class="row services-block services-04 m-5 text-right right">
+				<div class="number-overlay"> 04 </div>
+					<div class="title w-100">
+						<h2 class="pb-2">LEGAL AND <br> SECRETARIAL <br> SERVICES</h2>
+					</div>
+					<div class="content">
+						<p>Our experienced professionals handle tax, regulatory and legal compliance, so you can focus on building your company.</p>
+					</div>
+				</div>
+				<div class="row  services-block services-05 m-5 left">
+				<div class="number-overlay"> 05 </div>
+					<div class="title">
+						<h2 class="pb-2">WORKING CAPITAL <br> MANAGEMENT</h2>
+					</div>
+					<div class="content">
+						<p>Our intelligent voice and messaging-based digital expense ledgers makes it easy to track your company’s current spends, and our AI-engineered forecasting tools enables you to create a financial road map for the rest of your journey.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
