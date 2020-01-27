2382925836a095e17c2016425fb8a53bd58fbb4a
Jeromie <jeromie@ajency.in>
2020-01-27 13:14:09 +0530

    image fixes

--- wp-content/themes/toppeq/template-team.php
+++ wp-content/themes/toppeq/template-team.php
@@ -15,8 +15,8 @@ if (have_posts()) {
 <div class="team">
 	<h2 class="section-title f-25 text-uppercase py-md-4 py-2 pr-3"> Meet The Team </h2>
 	<div class="row team-image container w-980">
-		<div class="col-sm-6"><div class="rectangle-left"></div><img src="<?php echo get_template_directory_uri(); ?>/images/team-img1.png" class="w-100"></div>
-		<div class="col-sm-6"><div class="rectangle-right"></div><img src="<?php echo get_template_directory_uri(); ?>/images/team-img2.png" class="w-100"></div>
+		<div class="col-sm-6"><div class="rectangle-left"></div><img src="<?php echo get_template_directory_uri(); ?>/images/team-img1.png" alt="Team at Toppeq" title="Team at Toppeq" class="w-100"></div>
+		<div class="col-sm-6"><div class="rectangle-right"></div><img src="<?php echo get_template_directory_uri(); ?>/images/team-img2.png" alt="Team at Toppeq" title="Team at Toppeq" class="w-100"></div>
 	</div>
 
 	<section class="services">
@@ -24,36 +24,32 @@ if (have_posts()) {
 	        <div class="rotate--circle">
 	            <ul class="circle--rotate" id="circle--rotate">
 	                <li class="block">
-	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/large-img.png"></div>
+	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/large-img.png" alt="Nandini Sankar" title="Nandini Sankar"></div>
 	                </li>
 	                <li class="block">
-	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/hema.png"></div>
+	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/hema.png" alt="Hema Radhakrishna" title="Hema Radhakrishna"></div>
 	                </li>
 	                <li class="block">
-	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/ajay.png"></div>
+	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/aditya.png" alt="Aditya Yadav" title="Aditya Yadav"></div>
 	                </li>
 					<li class="block">
-	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/bhargavi.png"></div>
+	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/akhilesh.png" alt="Akhilesh Vyas" title="Akhilesh Vyas"></div>
 	                </li>
 					<li class="block">
-	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/aditya.png"></div>
+	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/kaartik.png" alt="Kaartik Malhotra" title="Kaartik Malhotra"></div>
 	                </li>
 					<li class="block">
-	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/akhilesh.png"></div>
+	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/ajay.png" alt="Ajay Khadke" title="Ajay Khadke"></div>
 	                </li>
-	                	<li class="block">
-	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/urmi.png"></div>
+	                <li class="block">
+	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/aayush.png" alt="Aayush Tainwala" title="Aayush Tainwala"></div>
 	                </li>
-	                	<li class="block">
-	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/aayush.png"></div>
+	                <li class="block">
+	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/bhargavi.png" alt="Bhargavi Swami" title="Bhargavi Swami"></div>
 	                </li>
-	                	<li class="block">
-	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/kaartik.png"></div>
+	                <li class="block">
+	                    <div class="icon"><img src="<?php echo get_template_directory_uri(); ?>/images/urmi.png" alt="Urmi Mehta" title="Urmi Mehta"></div>
 	                </li>
-
-
-
-
 	            </ul>
 	            <div class="animate-wrapper">
 	                <div class="animate">
@@ -92,36 +88,6 @@ if (have_posts()) {
 	                            <div class="animate-more">
 	                                <div class="animate-title">
 	                                	<div class="text-center">
-											<h5 class="name"> Ajay Khadke </h5>
-		                        			<p class="designation">Business Role</p>
-		                    			</div>
-	                                    <h4>Talent spotter and excellent team builder. Dreams of winning a PGA tournament. Recommends The Seventh Secret by Irving Wallace to all and sundry.</h4>
-	                                </div>
-	                            </div>
-	                        </div>
-	                    </div>
-	                </div>
-	                <div class="animate">
-	                    <div class="animate-img">
-	                        <div class="animate-img__in">
-	                            <div class="animate-more">
-	                                <div class="animate-title">
-	                                	<div class="text-center">
-											<h5 class="name"> Bhargavi Swami </h5>
-		                        			<p class="designation">Business Role</p>
-		                    			</div>
-	                                    <h4>Chief Happiness incharge with a penchant for people and stories. Can create a tune a minute and is taking over the world one Podcast at a time!</h4>
-	                                </div>
-	                            </div>
-	                        </div>
-	                    </div>
-	                </div>
-	                <div class="animate">
-	                    <div class="animate-img">
-	                        <div class="animate-img__in">
-	                            <div class="animate-more">
-	                                <div class="animate-title">
-	                                	<div class="text-center">
 											<h5 class="name"> Aditya Yadav </h5>
 		                        			<p class="designation">Business Role</p>
 		                    			</div>
@@ -142,6 +108,7 @@ if (have_posts()) {
 		                    			</div>
 	                                    <h4>Operates at the intersection of product and technology. Can whip up a mean vangi bhaat.</h4>
 	                                </div>
+	                                </div>
 	                            </div>
 	                        </div>
 	                    </div>
@@ -152,10 +119,25 @@ if (have_posts()) {
 	                            <div class="animate-more">
 	                                <div class="animate-title">
 	                                	<div class="text-center">
-											<h5 class="name"> Urmi Mehta </h5>
+											<h5 class="name"> Kaartik Malhotra </h5>
 		                        			<p class="designation">Business Role</p>
 		                    			</div>
-	                                    <h4>Hard taskmaster; always finds a way. Can solve Rubik’s Cube in XX seconds. Comedy movie buff.</h4>
+	                                    <h4>A potato trying to be a French fry</h4>
+	                                </div>
+	                            </div>
+	                        </div>
+	                    </div>
+	                </div>
+	                <div class="animate">
+	                    <div class="animate-img">
+	                        <div class="animate-img__in">
+	                            <div class="animate-more">
+	                                <div class="animate-title">
+	                                	<div class="text-center">
+											<h5 class="name"> Ajay Khadke </h5>
+		                        			<p class="designation">Business Role</p>
+		                    			</div>
+	                                    <h4>Talent spotter and excellent team builder. Dreams of winning a PGA tournament. Recommends The Seventh Secret by Irving Wallace to all and sundry.</h4>
 	                                </div>
 	                            </div>
 	                        </div>
@@ -182,10 +164,24 @@ if (have_posts()) {
 	                            <div class="animate-more">
 	                                <div class="animate-title">
 	                                	<div class="text-center">
-											<h5 class="name"> Kaartik Malhotra </h5>
+											<h5 class="name"> Bhargavi Swami </h5>
 		                        			<p class="designation">Business Role</p>
 		                    			</div>
-	                                    <h4>A potato trying to be a French fry</h4>
+	                                    <h4>Chief Happiness incharge with a penchant for people and stories. Can create a tune a minute and is taking over the world one Podcast at a time!</h4>
+	                            </div>
+	                        </div>
+	                    </div>
+	                </div>
+	                <div class="animate">
+	                    <div class="animate-img">
+	                        <div class="animate-img__in">
+	                            <div class="animate-more">
+	                                <div class="animate-title">
+	                                	<div class="text-center">
+											<h5 class="name"> Urmi Mehta </h5>
+		                        			<p class="designation">Business Role</p>
+		                    			</div>
+	                                    <h4>Hard taskmaster; always finds a way. Can solve Rubik’s Cube in XX seconds. Comedy movie buff.</h4>
 	                                </div>
 	                            </div>
 	                        </div>
