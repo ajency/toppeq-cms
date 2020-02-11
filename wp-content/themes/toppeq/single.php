783738830ef97f11022ac680751b13ec598bfac0
Vaibhav Arolkar <vaibhav.a@ajency.in>
2020-02-11 13:32:58 +0530

    Single blog page

--- wp-content/themes/toppeq/single.php
+++ wp-content/themes/toppeq/single.php
@@ -14,18 +14,30 @@ get_header();
                 while ( have_posts() ) :
                   the_post();
                 ?>
-                    <div class="single-post-featured-img text-center">
+                    <div class="single-post-featured-img post-featured-image text-center">
                         <?php
                             if ( has_post_thumbnail() ) :
-                            the_post_thumbnail( 'medium-large' );
+                                the_post_thumbnail( 'medium-large' );
                             endif;
                         ?>
                     </div>
                     <div class="header text-center my-2 py-1 px-0">
-                        <h2 class="single-post-title text-capitalize f-25 py-1 pl-3 font-weight-bold mb-0"><?php the_title(); ?></h2>
+                        <h2 class="section-title f-25 font-weight-bold text-uppercase mb-0 py-1 pl-3 "><?php the_title(); ?></h2>
+                        <?php 
+                            $author = get_the_author();
+                            $posted_in = '';
+                            foreach((get_the_category()) as $category) {
+                                $posted_in .= "<a href='../".str_replace(" ", "-", strtolower($category->cat_name))."' class='text-black'><strong class='text-underline text-capitalize'>".$category->cat_name."</strong></a> , ";
+                            }
+                            $posted_in = rtrim($posted_in,' , ');
+                        ?>
+                        <p>Posted in <?php echo $posted_in;?> by <?php the_author(); ?> on <?php echo get_the_date(); ?></p>            
                     </div>
                  
-                    <div class="single-post-content mb-20"><?php the_content(); ?></div>  
+                    <div class="page-content single-post-content mb-20">
+                        <?php the_content(); ?>
+                    </div>  
+
                     <div class="single-post-navigation row mt-30 mb-30">
                         <div class="col-md-6 previous">
                             <?php next_post_link('%link', 'Previous Post'); ?>
