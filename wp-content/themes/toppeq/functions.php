33ef02547d0f58d2bc0302f69c7c8144c3c8e4b5
Ankita <ankita@ajency.in>
2020-02-19 14:57:44 +0530

    updated login page logo code

--- wp-content/themes/toppeq/functions.php
+++ wp-content/themes/toppeq/functions.php
@@ -219,8 +219,3 @@ function my_login_logo_url() {
     return home_url();
 }
 add_filter( 'login_headerurl', 'my_login_logo_url' );
-
-function my_login_logo_url_title() {
-    return 'iv-advisors';
-}
-add_filter( 'login_headertitle', 'my_login_logo_url_title' );
