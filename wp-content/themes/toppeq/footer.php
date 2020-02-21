866ee73a16d5cc9594c75bf16fa0492097135e75
Ankita <ankita@ajency.in>
2020-02-21 12:23:40 +0530

    added defer to recaptcha script

--- wp-content/themes/toppeq/footer.php
+++ wp-content/themes/toppeq/footer.php
@@ -21,6 +21,7 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.js"></script>
+<script type='text/javascript' defer='defer' src='https://www.google.com/recaptcha/api.js?onload=recaptchaCallback&render=explicit&ver=2.0'></script>
 <?php wp_footer(); ?>
 </body>
 </html>
