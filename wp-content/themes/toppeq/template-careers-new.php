d2c1f394d5d0c56e342322c81eb5925a71ca8bfa
Vaibhav Arolkar <vaibhav.a@ajency.in>
2020-04-15 15:21:42 +0530

    Careers page

--- wp-content/themes/toppeq/template-careers-new.php
+++ wp-content/themes/toppeq/template-careers-new.php
@@ -12,16 +12,6 @@ get_header(); ?>
                 <div class="page-title">
                     <h2 class="section-title f-25 text-uppercase py-md-4 py-2 pr-3"> LIFE AT TOPPEQ </h2>
                 </div>
-                <div class="page-featured-image">
-                  <!--   <div class="row">
-                        <div class="col-7">
-                            <img src="<?php the_field('left_featured_image'); ?>" class="img-fluid c-image"/>
-                        </div>
-                        <div class="col-5">
-                            <img src="<?php the_field('right_featured_image'); ?>" class="img-fluid c-image"/>
-                        </div>
-                    </div> -->
-                </div>
                 <div class="page-content mb-5">
                     <?php the_content();?>
                 </div>
@@ -30,6 +20,7 @@ get_header(); ?>
                 </div>
                 <div class="openings">
                     <?php
+                        $j = 0;
                         if( have_rows('jobs') ):
                             while ( have_rows('jobs') ) : the_row();?>
                                 <div class="openings-row">
@@ -43,16 +34,17 @@ get_header(); ?>
                                                 $i = 0;
                                                 if( have_rows('jobs_repeater_field') ):
                                                     while ( have_rows('jobs_repeater_field') ) : the_row();?> 
-                                                        <div class="accordion" id="accordionOpenings">
+                                                        <div class="accordion" id="accordionOpenings-<?php echo $j;?>">
                                                             <div class="card">
                                                                 <div class="card-header" id="headingOne">
-                                                                    <h5 class="mb-0" data-toggle="collapse" data-target="#collapse-<?php echo $i;?>" aria-expanded="false" aria-controls="collapse-<?php echo $i;?>">
+                                                                    <h5 class="mb-0" data-toggle="collapse" data-target="#collapse-<?php echo $j;?>-<?php echo $i;?>" aria-expanded="false" aria-controls="collapse-<?php echo $j;?>-<?php echo $i;?>">
                                                                         <?php echo the_sub_field('job_title');?>
                                                                     </h5>
                                                                 </div>
-                                                                <div id="collapse-<?php echo $i;?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordionOpenings">
+                                                                <div id="collapse-<?php echo $j;?>-<?php echo $i;?>" class="collapse" aria-labelledby="headingOne" data-parent="#accordionOpenings-<?php echo $j;?>">
                                                                     <div class="card-body">
                                                                         <?php echo the_sub_field('job_content');?>
+                                                                        <button class="btn btn-sm btn-outline-primary mt-2 rounded-0" data-toggle="modal" data-target="#exampleModalCenter">Apply Now</button>
                                                                     </div>
                                                                 </div>
                                                             </div>
@@ -66,10 +58,27 @@ get_header(); ?>
                                         </div>
                                     </div>
                                 </div>
-                            <?php endwhile;
+                            <?php 
+                            $j++;
+                            endwhile;
                         else :
                         endif;
                     ?>
+                </div>
+                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered modal-lg modal-careers" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h4 class="modal-title fot-weight-light" id="exampleModalLongTitle">Position for </h4>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                   <img width="20" src="<?php echo get_template_directory_uri(); ?>/images/cross-on-hover.svg" class="img-fluid" />
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <?php echo do_shortcode( '[formidable id=4 title=false description=false]' );?>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         <?php }
